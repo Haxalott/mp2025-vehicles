5621e3998cb9ab25b4155041c6dc81230928755a
Haxalott's Developments <haxalott@proton.me>
2025-07-12 16:01:12 +1000

    Upload files

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'Vehicle data & stream files from game build 3570 for other game builds'
+author 'Haxalott'
+
+version '1.0.0'
+
+files {
+    'data/vehicles.meta',
+    'data/handling.meta',
+    'data/carcols.meta',
+    'data/carvariations.meta',
+    'data/vehiclelayouts.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
